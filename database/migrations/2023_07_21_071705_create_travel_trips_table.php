8690f88602b68dda2fd6c06a2b74d9d7b0dc004e
Ferdy Hahan Pradana <75787853+ferdyhape@users.noreply.github.com>
2023-07-21 15:19:09 +0700

    add seat and image trip to traveltrip table

--- database/migrations/2023_07_21_071705_create_travel_trips_table.php
+++ database/migrations/2023_07_21_071705_create_travel_trips_table.php
@@ -20,6 +20,8 @@ return new class extends Migration
             $table->string('departure_city', 50);
             $table->string('destination_city', 50);
             $table->decimal('price', 10, 2);
+            $table->integer('seat')->default(0);
+            $table->string('image_trip')->default('trip/default.png');
             $table->foreignUuid('travel_company_id')->constrained('travel_companies')->onDelete('cascade');
             $table->timestamps();
         });
