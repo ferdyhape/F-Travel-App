6988b612b44781a0e01a607e9063ed29a8b98fc2
Ferdy Hahan Pradana <75787853+ferdyhape@users.noreply.github.com>
2023-07-22 18:46:19 +0700

    change datatype to uuid on passanger table

--- database/migrations/2023_07_21_072248_create_passengers_table.php
+++ database/migrations/2023_07_21_072248_create_passengers_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('passengers', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary;
             $table->foreignUuid('booking_id')->constrained('bookings')->onDelete('cascade');
             $table->string('name');
             $table->string('phone_number', 13);
