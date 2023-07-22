c6cc7cb66fea07afcc771b11381c5caa841f7264
Ferdy Hahan Pradana <75787853+ferdyhape@users.noreply.github.com>
2023-07-22 14:03:44 +0700

    add checkCompany middleware and time format validator

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -65,5 +65,6 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'check.company' => \App\Http\Middleware\CheckCompanyMiddleware::class,
     ];
 }
--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Validator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,6 +24,12 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        //
+        Validator::extend('time_format', function ($attribute, $value, $parameters, $validator) {
+            // Define the regex pattern for the desired time format (HH:mm)
+            $pattern = '/^([0-1]?[0-9]|2[0-3]):[0-5][0-9]$/';
+
+            // Perform the validation using preg_match
+            return preg_match($pattern, $value);
+        });
     }
 }
