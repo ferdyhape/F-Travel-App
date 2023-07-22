100a7abe83a7b46f18ec95c5801c07929b266183
Ferdy Hahan Pradana <75787853+ferdyhape@users.noreply.github.com>
2023-07-22 18:45:14 +0700

    remove date from booking table

--- database/migrations/2023_07_21_072011_create_bookings_table.php
+++ database/migrations/2023_07_21_072011_create_bookings_table.php
@@ -17,7 +17,6 @@ return new class extends Migration
             $table->uuid('id')->primary();
             $table->foreignUuid('travel_trip_id')->constrained('travel_trips')->onDelete('cascade');
             $table->foreignUuid('user_id')->constrained('users')->onDelete('cascade');
-            $table->date('date');
             $table->integer('total_passenger');
             $table->decimal('total_price', 10, 2);
             $table->enum('status', ['Pending', 'Success', 'Canceled']);
